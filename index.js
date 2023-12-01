3bec2a554c0cdefd5fc8a3ffa14e7edc0442eee3
GabrielOchieng <ogingagabriel@gmail.com>
2023-12-01 12:43:29 +0300

    update

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use(
 	cors({
 		origin: ["http://localhost:3000", "https://elck-cdc.onrender.com/"],
 		methods: ["GET", "POST", "PUT", "DELETE"],
-		credentials: false,
+		credentials: true,
 	})
 );
 
