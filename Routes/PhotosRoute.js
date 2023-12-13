a562ba54d791a5eeef495c6fb760b90edcfbbd51
GabrielOchieng <ogingagabriel@gmail.com>
2023-12-13 09:01:16 +0300

    update

--- Routes/PhotosRoute.js
+++ Routes/PhotosRoute.js
@@ -18,7 +18,10 @@ const upload = multer({ storage: storage });
 // Route for Save a new Photo
 router.post("/", upload.single("image"), async (request, response) => {
 	try {
-		if (!request.body.image || !request.body.description) {
+		if (
+			// !request.body.image ||
+			!request.body.description
+		) {
 			return response.status(400).send({
 				message: "Send all required fields: image, description",
 			});
